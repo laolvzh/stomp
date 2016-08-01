a6ff3e4f7a8dd863d2f20dcb46e10a810edad627
Kristina <isitiriss@gmail.com>
2016-08-01 18:07:22 +0300

    formatted deb-package

--- stompd/deb-package/make-deb.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-dpkg-deb --build go-stomp-server
-mv go-stomp-server.deb go-stomp-server_1.0-1_all.deb
-scp go-stomp-server_1.0-1_all.deb k@192.168.240.25:~/
---
+++ stompd/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+./configure --prefix=/
+make
+sudo checkinstall -D --pkgversion=0.6 --pkgname=go-stomp-server \
+       --maintainer="Kristina Kovalevskaya isitiriss@gmail.com" --autodoinst=yes \
+       --spec=ABOUT.md --provides=""
+
+RETVAL=$?
+[ $RETVAL -eq 0 ] && echo Success
+[ $RETVAL -ne 0 ] && echo Failure
