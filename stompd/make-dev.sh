3e95bb46e6949d98acb04618d7027cfff0e5a798
Kristina <isitiriss@gmail.com>
2016-08-02 11:54:05 +0300

    fixed bugs

--- stompd/make-dev.sh
+++ stompd/make-dev.sh
@@ -3,9 +3,9 @@ set -e
 autoconf
 ./configure --prefix=/
 make
-sudo checkinstall -D --pkgversion=0.6 --pkgname=go-stomp-server \
+sudo checkinstall -D --pkgversion=0.6.1 --pkgname=go-stomp-server \
        --maintainer="Kristina Kovalevskaya isitiriss@gmail.com" --autodoinst=yes \
-       --spec=ABOUT.md --provides=""
+       --spec=ABOUT.md --provides="" --pkgsource=go-stomp-server
 
 RETVAL=$?
 [ $RETVAL -eq 0 ] && echo Success
