d6048745f4474256d0d4678b593920bb4ff11396
Kristina <isitiriss@gmail.com>
2016-08-09 19:56:09 +0300

    needed files was deleted because i can't in git merge

---
+++ stompd/go-stomp-server.go
@@ -0,0 +1,60 @@
+/*
+A simple, stand-alone STOMP server.
+
+TODO: graceful shutdown
+
+TODO: UNIX daemon functionality
+
+TODO: Windows service functionality (if possible?)
+
+TODO: Logging options (syslog, windows event log)
+*/
+package main
+
+import _ "github.com/KristinaEtc/slflog"
+
+import (
+	"net"
+
+	conf "github.com/KristinaEtc/config"
+	"github.com/go-stomp/stomp/server"
+	"github.com/go-stomp/stomp/server/auth"
+	"github.com/ventu-io/slf"
+)
+
+var configFile string
+
+var log = slf.WithContext("go-stompd-server.go")
+
+// GlobalConf is a struct with global options,
+// like server address and config auth filename
+type GlobalConf struct {
+	ListenAddr string
+}
+
+// ConfFile is a file with all program options
+type ConfFile struct {
+	Global GlobalConf
+}
+
+var globalOpt = ConfFile{Global: GlobalConf{ListenAddr: "localhost:61614"}}
+
+func main() {
+
+	conf.ReadGlobalConfig(&globalOpt, "GlobalConf")
+
+	// TODO: add Close method!!
+	//defer slflog.Close()
+
+	l, err := net.Listen("tcp", globalOpt.Global.ListenAddr)
+	if err != nil {
+		log.WithCaller(slf.CallerShort).Fatalf("Failed to listen: %s", err.Error())
+	}
+	defer func() { l.Close() }()
+
+	a := auth.NewAuth(conf.GetConfigFilename())
+
+	log.Debugf("listening on %v %s", l.Addr().Network(), l.Addr().String())
+	log.Error("-----------------------------------------------")
+	server.Serve(l, a)
+}
