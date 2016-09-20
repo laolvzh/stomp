47c1211ff2317af01f28b6da89f174fa17992b5f
metametaclass <metametaclass@gmail.com>
2016-09-20 14:45:25 +0300

    #20002 user lowercase names

--- server/status/status.go
+++ server/status/status.go
@@ -22,22 +22,21 @@ type QueueStatus struct {
 }
 
 type ServerStatus struct {
-	Clients []ServerClientStatus
-	Queues  []QueueStatus
-	Topics  []QueueStatus
-	Time    string
-	Type    string `json:type`
-	Id      string `json:id`
-	Name    string `json:name`
-
-	Subtype      string `json:subtype`
-	Subsystem    string `json:subsystem`
-	ComputerName string `json:computername`
-	UserName     string `json:username`
-	ProcessName  string `json:processname`
-	Version      string `json:version`
-	Pid          int    `json:pid`
-	Tid          int    `json:tid`
-	Severity     int    `json:severity`
-	Message      string `json:message`
+	Clients      []ServerClientStatus
+	Queues       []QueueStatus
+	Topics       []QueueStatus
+	Time         string
+	Type         string `"json:type"`
+	Id           string `"json:id"`
+	Name         string `"json:name"`
+	Subtype      string `json:"subtype"`
+	Subsystem    string `json:"subsystem"`
+	ComputerName string `json:"computer"`
+	UserName     string `json:"user"`
+	ProcessName  string `json:"process"`
+	Version      string `json:"version"`
+	Pid          int    `json:"pid"`
+	Tid          int    `json:"tid"`
+	Severity     int    `json:"severity"`
+	Message      string `json:"message"`
 }
